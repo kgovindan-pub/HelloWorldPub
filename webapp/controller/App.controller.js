50f6491fead187fb74efadc6d3759a6204e8e624
Kirubakaran Thillai Govindan <kgovindan@scotts.com>
2025-02-02 01:46:45 +0000

    Initial commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("kiru.btp.helloworldui5.controller.App", {
+      onInit() {
+      }
+  });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"kirubtp/helloworldui5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
